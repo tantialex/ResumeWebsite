6bb25fd9239825e34a98b5b80de63c62aa3f839c
tantialex <atanti98@gmail.com>
2016-03-07 22:57:27 +0100

    Updated project boxes
    Added background images & animations for project boxes

--- jscript.js
+++ jscript.js
@@ -1,7 +1,7 @@
-document.getElementsByClassName("projectBox").onclick = ProjectBoxOpen;
+document.getElementsByClassName("project-box-wrapper").onclick = ProjectBoxOpen;
 
 function ProjectBoxOpen(elem){
-	var boxes = document.getElementsByClassName('projectBox');
+	var boxes = document.getElementsByClassName('project-box-wrapper');
         for (i = 0; i < boxes.length; i++) {
         	if(boxes[i].classList.contains('opened')){
 	       		boxes[i].classList.remove('opened');   
@@ -11,4 +11,12 @@ function ProjectBoxOpen(elem){
         
 	elem.classList.remove('closed');
 	elem.classList.add('opened');
+}
+
+window.onload = function ProjectBoxBackground() {
+    var contents = document.getElementsByClassName('project-box-content');
+        for (i=0; i<contents.length; i++) {
+            var background_path = contents[i].getAttribute("dataPath");
+            contents[i].style.backgroundImage = "url('"+ background_path +"')";
+        }
 }
